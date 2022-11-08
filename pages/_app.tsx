2ecc88d566a26a6baeac8ed99770e2a9d46c1f6a
Jennifer Chow <jennego@gmail.com>
2022-11-07 23:42:32 -0800

    got to the point where I'm getting data from fauna

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Nav from "../components/nav";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../gglClient";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Nav />
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  );
 }
