f7835637bfd3c9db13b2659590912ce332f13eac
Jennifer Chow <jennego@gmail.com>
2022-11-09 23:26:43 -0800

    add products

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { gql, useQuery, ApolloProvider } from "@apollo/client";
+import ProductList from "../components/productList";
 
 const GET_SHOP = gql`
   query {
@@ -16,10 +17,31 @@ const GET_SHOP = gql`
     }
   }
 `;
+
+const GET_ALL_PRODUCTS = gql`
+  query {
+    getAllProducts {
+      data {
+        name
+        price
+        _id
+        description
+        shop {
+          _id
+        }
+      }
+    }
+  }
+`;
+
 const Home = () => {
-  const { data } = useQuery(GET_SHOP);
+  const { data } = useQuery(GET_ALL_PRODUCTS);
   console.log(data);
-  return <div></div>;
+  return (
+    <div>
+      <ProductList products={data.getAllProducts.data} />
+    </div>
+  );
 };
 
 export default Home;
